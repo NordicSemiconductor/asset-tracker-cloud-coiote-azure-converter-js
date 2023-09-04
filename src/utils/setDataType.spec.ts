5cd2c083a44954da0646279e9191085a70b629ed
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-09-04 12:28:30 +0200

    test: remove jest remove jest from setDatatype method. Use node:test instead

--- src/utils/setDataType.spec.ts
+++ src/utils/setDataType.spec.ts
@@ -1,28 +1,33 @@
+import { describe, test as it } from 'node:test'
+import assert from 'node:assert'
 import { setDataType } from './setDataType.js'
+import type { List, Value } from 'src/converter.js'
 
-describe('setDataType', () => {
-	it('should return string if not data type is specified (default option)', () => {
-		expect(setDataType({ value: 1 })).toBe('1')
-	})
+void describe('setDataType', () => {
+	void it('should return string if not data type is specified (default option)', () =>
+		assert.equal(setDataType({ value: 1 }), '1'))
 
-	it('should transform input in array', () => {
+	void it('should transform input in array', () => {
 		const list = {
 			'0': { value: 8 },
 			'1': { value: 10 },
 			attributes: { dim: '2' },
 		}
 		const result = setDataType(list)
-		expect(result).toMatchObject([8, 10])
-		if (Array.isArray(result)) {
-			expect(result.length).toBe(Number(list.attributes.dim))
-		}
+		assert.deepEqual(result, [8, 10])
+		if (Array.isArray(result))
+			assert.equal(result.length, Number(list.attributes.dim))
 	})
 
-	it.each([
+	for (const [input, dataType, expected] of [
 		[{ value: 'a' }, 'string', 'a'],
 		[{ value: 1 }, 'integer', 1],
 		[{ value: 0 }, 'boolean', false],
-	])(`should transform '%j' in %s`, (input, dataType, expected) => {
-		expect(setDataType(input, dataType)).toBe(expected)
-	})
+	]) {
+		void it(`should transform '${JSON.stringify(input)}' in ${dataType}`, () =>
+			assert.equal(
+				setDataType(input as List | Value, dataType as string),
+				expected,
+			))
+	}
 })
