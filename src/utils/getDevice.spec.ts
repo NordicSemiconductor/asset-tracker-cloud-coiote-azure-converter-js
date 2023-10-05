ab80fd895788a976709041e5f6120bef6eb94690
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-05 13:47:27 +0200

    test: statements for get device method

---
+++ src/utils/getDevice.spec.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+
+void describe('getDevice', () => {
+	void it(`should create the LwM2M object 'Device' (3) from the object '3' reported by Coiote`, () => {
+		
+	})
+
+	void it(`should return a warning if the object '3' reported by Coiote is not defined`, () => {
+       
+    })
+
+	void it(`should return an error if the result of the conversion does not meet the schema definition of LwM2M obejct 3`, () => {
+       
+    })
+
+})
