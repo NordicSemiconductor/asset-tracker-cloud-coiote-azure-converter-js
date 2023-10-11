73feef69ebd1287c3b686ab09ed9db3def631510
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-11 16:06:44 +0200

    feat: get timestamp from metadata

---
+++ src/utils/getTimestampFromMetadata.spec.ts
@@ -0,0 +1,126 @@
+import { describe, test as it } from 'node:test'
+import assert from 'node:assert'
+import {
+	getTimestampFromMetadata,
+	parseTime,
+	type Metadata,
+    type Instance,
+    type Obj,
+} from './getTimestampFromMetadata'
+import { Temperature_3303_urn } from '@nordicsemiconductor/lwm2m-types'
+
+
+void describe('getTimestampFromMetadata', () => {
+	void it(`should get timestamp from the resource value reported in device twin`, () => {
+		/**
+		 * This is the second level of Timestamp hierarchy.
+		 * This applies when the resource 5518 is not defined in the object being evaluated
+		 * @see {@link ../../adr/004-timestamp-hierarchy.md}
+		 */
+		const timeToReport = '2023-10-18T14:39:11.9414162Z'
+		const timeToReportParsed = parseTime(timeToReport)
+		const metadata: Metadata = {
+			$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			lwm2m: {
+				'3303': {
+					'0': {
+						// ...
+						// others resources' metadata value
+						// ..
+						'5700': {
+							$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+							value: {
+								$lastUpdated: timeToReport,
+							},
+						},
+						$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+					},
+					$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+				},
+				$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			},
+		}
+
+		assert.equal(
+			getTimestampFromMetadata(Temperature_3303_urn, metadata),
+			timeToReportParsed,
+		)
+	})
+
+	void it(`should get timestamp from the instance value reported in device twin`, () => {
+		/**
+		 * This is the third level of Timestamp hierarchy.
+		 * This applies when the reported value for resource 5570 is not defined in device twin.
+		 * @see {@link ../../adr/004-timestamp-hierarchy.md}
+		 */
+        const timeToReport = '2023-10-18T14:39:11.9414162Z'
+		const timeToReportParsed = parseTime(timeToReport)
+		const metadata: Metadata = {
+			$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			lwm2m: {
+				'3303': {
+					'0': {
+						'5601': {
+							$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+							value: {
+								$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+							},
+						},
+						$lastUpdated: timeToReport,
+					},
+					$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+				},
+				$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			},
+		}
+		assert.equal(
+			getTimestampFromMetadata(Temperature_3303_urn, metadata),
+			timeToReportParsed,
+		)
+	})
+
+	void it(`should get timestamp from the object value reported in device twin`, () => {
+		/**
+		 * This is the third level of Timestamp hierarchy.
+		 * This applies when the reported value for instance is not defined in device twin.
+		 * @see {@link ../../adr/004-timestamp-hierarchy.md}
+		 */
+        const timeToReport = '2023-10-18T14:39:11.9414162Z'
+		const timeToReportParsed = parseTime(timeToReport)
+		const metadata: Metadata = {
+			$lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			lwm2m: {
+				'3303': {
+					'0': {} as Instance,
+					$lastUpdated: timeToReport,
+				},
+                $lastUpdated: '2023-08-18T14:39:11.9414162Z',
+			},
+		}
+		assert.equal(
+			getTimestampFromMetadata(Temperature_3303_urn, metadata),
+			timeToReportParsed,
+		)
+	})
+
+	void it(`should get timestamp from the LwM2M value reported in device twin`, () => {
+		/**
+		 * This is the fourth level of Timestamp hierarchy.
+		 * This applies when the reported value for object is not defined in device twin.
+		 * @see {@link ../../adr/004-timestamp-hierarchy.md}
+		 */
+        const timeToReport = '2023-10-18T14:39:11.9414162Z'
+		const timeToReportParsed = parseTime(timeToReport)
+		const metadata: Metadata = {
+			$lastUpdated: timeToReport,
+			lwm2m: {
+				'3303': {} as Obj,
+				$lastUpdated: timeToReport,
+			},
+		}
+		assert.equal(
+			getTimestampFromMetadata(Temperature_3303_urn, metadata),
+			timeToReportParsed,
+		)
+	})
+})
---
+++ src/utils/getTimestampFromMetadata.ts
@@ -0,0 +1,82 @@
+import {
+	parseURN,
+	type Humidity_3304_urn,
+	type Pressure_3323_urn,
+	type Temperature_3303_urn,
+} from '@nordicsemiconductor/lwm2m-types'
+
+export type Metadata = {
+	$lastUpdated: string
+	lwm2m: LwM2M_Metadata
+}
+
+export type LwM2M_Metadata = {
+	[key: `${number}`]: Obj // object ID : object
+	$lastUpdated: string
+}
+
+export type Obj = {
+	[key: `${number}`]: Instance // Instance id : instance
+	$lastUpdated: string
+}
+
+export type Instance = {
+	[key: `${number}`]: Resource // Resource id : resource
+	$lastUpdated: string
+}
+
+type Resource = {
+	$lastUpdated: string
+	value: {
+		$lastUpdated: string
+	}
+}
+
+/**
+ *
+ */
+export const parseTime = (time: string) => new Date(time).getTime()
+
+/**
+ * metadata example
+ * @see https://github.com/MLopezJ/asset-tracker-lwm2m-js/issues/2
+ */
+
+/**
+ * objectUrn only could be temperature, humidity or pressure because timestamp hierarchy only applies to them
+ * @see {@link ../../adr/004-timestamp-hierarchy.md}
+ */
+export const getTimestampFromMetadata = (
+	objectUrn:
+		| typeof Temperature_3303_urn
+		| typeof Humidity_3304_urn
+		| typeof Pressure_3323_urn,
+	metadata: Metadata,
+): number | void => {
+	const { ObjectID } = parseURN(objectUrn) 
+	const object = metadata.lwm2m[`${ObjectID as unknown as number}`]
+	
+	if (object !== undefined) {
+
+		if (object['0'] !== undefined) {
+
+			// value from the resource 5700 of the given object
+			// default instance is the first one
+			const level2 = object['0']['5700']?.value.$lastUpdated
+			if (level2 !== undefined) return parseTime(level2)
+
+			// value from the instance of the given object
+			// default instance is the first one
+			const level3 = object['0']?.$lastUpdated
+			if (level3 !== undefined) return parseTime(level3)
+		}
+
+		// value of the given object
+		const level4 = object?.$lastUpdated
+		if (level4 !== undefined) return parseTime(level4)
+	}
+
+	// value of the lwm2m property in the device twin
+	const level5 = metadata.lwm2m.$lastUpdated
+	if (level5 !== undefined) return parseTime(level5)
+}
