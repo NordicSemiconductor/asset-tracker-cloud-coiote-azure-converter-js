b839f35cd0339b1da7511f51e0815550e0195f9f
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-18 13:52:14 +0200

    fix: rename variables
    Improve readability of function 'getDevice'

--- src/utils/getDevice.ts
+++ src/utils/getDevice.ts
@@ -1,21 +1,22 @@
 import { Device_3_urn, type Device_3 } from '@nordicsemiconductor/lwm2m-types'
 import { UndefinedCoioteObjectWarning } from './UndefinedCoioteObjectWarning.js'
 import { validateLwM2MFormat } from './validateLwM2MFormat.js'
-import type { ConversionResult, Instance } from 'src/converter.js'
-import { removeCoioteFormatFromSingleInstanceObj } from './removeCoioteFormatFromSingleInstanceObj.js'
+import type {
+	ConversionResult,
+	Instance as CoioteFormat,
+} from 'src/converter.js'
+import { removeCoioteFormatFromSingleInstanceObj as removeCoioteFormatFrom } from './removeCoioteFormatFromSingleInstanceObj.js'
 
 /**
  * Convert to LwM2M Device object (id 3) from the object 3 reported by Coiote
  */
 export const getDevice = (
-	objectWithCoioteFormat?: Instance,
+	object?: CoioteFormat,
 ): ConversionResult<Device_3> => {
-	if (objectWithCoioteFormat === undefined)
+	if (object === undefined)
 		return { error: new UndefinedCoioteObjectWarning(Device_3_urn) }
 
-	const device = removeCoioteFormatFromSingleInstanceObj(
-		objectWithCoioteFormat,
-	) as Device_3 // TODO: return the type in the function
+	const device = removeCoioteFormatFrom(object)
 
-	return validateLwM2MFormat(Device_3_urn, device)
+	return validateLwM2MFormat(Device_3_urn, device as Device_3)
 }
