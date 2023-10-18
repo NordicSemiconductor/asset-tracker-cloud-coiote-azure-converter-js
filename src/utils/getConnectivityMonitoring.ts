457e43b2fadf5a92f68d7b4a60dc441c7779d0e1
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-18 13:54:30 +0200

    fix: rename variables
    Improve readability of function 'getConnectivityMonitoring'

--- src/utils/getConnectivityMonitoring.ts
+++ src/utils/getConnectivityMonitoring.ts
@@ -4,26 +4,27 @@ import {
 } from '@nordicsemiconductor/lwm2m-types'
 import { UndefinedCoioteObjectWarning } from './UndefinedCoioteObjectWarning.js'
 import { validateLwM2MFormat } from './validateLwM2MFormat.js'
-import type { ConversionResult, Instance } from 'src/converter.js'
-import { removeCoioteFormatFromSingleInstanceObj } from './removeCoioteFormatFromSingleInstanceObj.js'
+import type {
+	ConversionResult,
+	Instance as CoioteFormat,
+} from 'src/converter.js'
+import { removeCoioteFormatFromSingleInstanceObj as removeCoioteFormatFrom } from './removeCoioteFormatFromSingleInstanceObj.js'
 
 /**
  * Convert to LwM2M Connectivity Monitoring object (id 4) from the object 4 reported by Coiote
  */
 export const getConnectivityMonitoring = (
-	objectWithCoioteFormat?: Instance,
+	object?: CoioteFormat,
 ): ConversionResult<ConnectivityMonitoring_4> => {
-	if (objectWithCoioteFormat === undefined)
+	if (object === undefined)
 		return {
 			error: new UndefinedCoioteObjectWarning(ConnectivityMonitoring_4_urn),
 		}
 
-	const connectivityMonitoring = removeCoioteFormatFromSingleInstanceObj(
-		objectWithCoioteFormat,
-	) as ConnectivityMonitoring_4 // TODO: return the type in the function
+	const connectivityMonitoring = removeCoioteFormatFrom(object)
 
 	return validateLwM2MFormat(
 		ConnectivityMonitoring_4_urn,
-		connectivityMonitoring,
+		connectivityMonitoring as ConnectivityMonitoring_4,
 	)
 }
