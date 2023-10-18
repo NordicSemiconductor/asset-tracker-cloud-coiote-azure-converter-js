6e337f1c832b51dd7897444b91c9049ea7131e79
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-18 13:48:55 +0200

    fix: rename variables
    Improve readability of function 'getLocation'

--- src/utils/getLocation.ts
+++ src/utils/getLocation.ts
@@ -1,21 +1,22 @@
 import { Location_6_urn, type Location_6 } from '../schemas/index.js'
 import { UndefinedCoioteObjectWarning } from './UndefinedCoioteObjectWarning.js'
 import { validateLwM2MFormat } from './validateLwM2MFormat.js'
-import type { ConversionResult, Instance } from 'src/converter.js'
-import { removeCoioteFormatFromSingleInstanceObj } from './removeCoioteFormatFromSingleInstanceObj.js'
+import type {
+	ConversionResult,
+	Instance as CoioteFormat,
+} from 'src/converter.js'
+import { removeCoioteFormatFromSingleInstanceObj as removeCoioteFormatFrom } from './removeCoioteFormatFromSingleInstanceObj.js'
 
 /**
  * Convert to LwM2M Location object (id 6) from the object 6 reported by Coiote
  */
 export const getLocation = (
-	objectWithCoioteFormat?: Instance,
+	object?: CoioteFormat,
 ): ConversionResult<Location_6> => {
-	if (objectWithCoioteFormat === undefined)
+	if (object === undefined)
 		return { error: new UndefinedCoioteObjectWarning(Location_6_urn) }
 
-	const location = removeCoioteFormatFromSingleInstanceObj(
-		objectWithCoioteFormat,
-	) as Location_6 // TODO: return the type in the function
+	const maybeLocation = removeCoioteFormatFrom(object)
 
-	return validateLwM2MFormat(Location_6_urn, location)
+	return validateLwM2MFormat(Location_6_urn, maybeLocation as Location_6)
 }
