e967bf7dca5b6509144ef2b9a002030b54c9051f
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-19 15:15:28 +0200

    fix: remove coiote format from Config object

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -1,31 +1,25 @@
 import { warning } from './UndefinedCoioteObjectWarning.js'
-import type { ConversionResult, Instance } from 'src/converter.js'
+import type {
+	ConversionResult,
+	Instance as CoioteFormat,
+} from 'src/converter.js'
 import {
 	Config_50009_urn,
 	type Config_50009,
 } from 'src/schemas/Config_50009.js'
-import { setCustomFormat } from './setCustomFormat.js'
+import { removeCoioteFormatFromSingleInstanceObj as removeCoioteFormatFrom } from './removeCoioteFormatFromSingleInstanceObj.js'
 
 /**
  * Convert to LwM2M Config object (id 50009) from the object 50009 reported by Coiote
  */
 export const getConfig = (
-	config_coiote?: Instance,
+	object?: CoioteFormat,
 ): ConversionResult<Config_50009> => {
-	if (config_coiote === undefined) return warning(Config_50009_urn)
+	if (object === undefined) return warning(Config_50009_urn)
 
-	const config = setCustomFormat({
-		[`${Config_50009_urn}`]: config_coiote,
-	})
+	const maybeConfig = removeCoioteFormatFrom(object)
 
-	// FIXME: this check is not working.
 	// TODO: validate the veracity of the new format
-	/*
-	const validatedLwM2MConfig = validateLwM2MFormat(config)
 
-	if ('error' in validatedLwM2MConfig)
-		return { error: validatedLwM2MConfig.error }
-	*/
-
-	return { result: config[Config_50009_urn] as unknown as Config_50009 }
+	return { result: maybeConfig as unknown as Config_50009 }
 }
