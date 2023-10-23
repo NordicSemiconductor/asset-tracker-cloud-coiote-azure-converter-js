e824ac1ff3a7b99821f4494da2bd07a8dafab3a3
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-23 10:50:48 +0200

    fix: refactor method
    refactor method by combine validation process.
    
    'validateWithTypebox' was removed and the logic of that method was added inside 'validateAgainstSchema'

--- src/utils/validateAgainstSchema.ts
+++ src/utils/validateAgainstSchema.ts
@@ -3,6 +3,8 @@ import Ajv, { type ErrorObject } from 'ajv'
 import { ValidationError } from './ValidationError.js'
 import type { UndefinedCoioteObjectWarning } from './UndefinedCoioteObjectWarning.js'
 
+const ajv = new Ajv()
+
 /**
  * Check if object follow the schema definition
  */
@@ -12,39 +14,13 @@ export const validateAgainstSchema = <T extends TSchema>(
 ):
 	| { result: Static<typeof schema> }
 	| { error: ValidationError | UndefinedCoioteObjectWarning } => {
-	const validatedObject = validateWithTypebox(object, schema)
-	if ('errors' in validatedObject) {
-		return {
-			error: new ValidationError(validatedObject.errors),
-		}
-	}
-
-	return { result: validatedObject.valid }
-}
-
-const ajv = new Ajv()
-
-/**
- * Use typebox to check if object follow the schema definition
- *
- * TODO: add logic in validateAgainstSchema
- */
-const validateWithTypebox = <T extends TSchema>(
-	object: unknown,
-	schema: T,
-):
-	| {
-			errors: ErrorObject[]
-	  }
-	| {
-			valid: unknown
-	  } => {
 	const v = ajv.compile(schema)
 	const valid = v(object)
+
 	if (valid !== true) {
 		return {
-			errors: v.errors as ErrorObject[],
+			error: new ValidationError(v.errors as ErrorObject[]),
 		}
 	}
-	return { valid: object }
+	return { result: object }
 }
