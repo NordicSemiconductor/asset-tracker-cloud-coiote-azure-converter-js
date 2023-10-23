935aa144ffabb4fcc0333fc3624e1f8abdda3195
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-23 15:45:59 +0200

    test: remove tests
    'schema' make no part of 'removeCoioteFormatFromSingleInstanceObj' context. For that reason, tests related to it are remove.

--- src/utils/removeCoioteFormatFromSingleInstanceObj.spec.ts
+++ src/utils/removeCoioteFormatFromSingleInstanceObj.spec.ts
@@ -3,7 +3,7 @@ import assert from 'node:assert'
 import { removeCoioteFormatFromSingleInstanceObj } from './removeCoioteFormatFromSingleInstanceObj.js'
 
 void describe('removeCoioteFormatFromSingleInstanceObj', () => {
-	void it('should remove coiote format from object using schema definition', () => {
+	void it('should remove coiote format from single instance object', () => {
 		const object = {
 			'0': {
 				'0': {
@@ -36,69 +36,5 @@ void describe('removeCoioteFormatFromSingleInstanceObj', () => {
 		}
 
 		assert.deepEqual(removeCoioteFormatFromSingleInstanceObj(object), result)
-	})
-
-	void it('should remove coiote format from object even when input does not follow required values in the schema definition', () => {
-		const object = {
-			'0': {
-				'0': {
-					value: 'Nordic Semiconductor ASA',
-				},
-				'1': {
-					value: 'Thingy:91',
-				},
-				/*
-				// this reource is required in order with the schema definition
-				'11': {
-					'0': {
-						value: 0,
-					},
-					attributes: {
-						dim: '1',
-					},
-				},
-				*/
-
-				'16': {
-					value: 'UQ',
-				},
-			},
-		}
-		const result = {
-			'0': 'Nordic Semiconductor ASA',
-			'1': 'Thingy:91',
-			// '11': [0],
-			'16': 'UQ',
-		}
-
-		assert.deepEqual(removeCoioteFormatFromSingleInstanceObj(object), result)
-	})
-
-	void it('should remove coiote format even when result does not follow the schema definition', () => {
-		const object = {
-			'0': {
-				'0': {
-					value: 'Nordic Semiconductor ASA',
-				},
-				'1': {
-					value: 123, // this should be an string in order with schema definition
-				},
-				'11': {
-					value: 'Thingy:91', // this value should be a list in order with schema definition
-				},
-				'16': {
-					value: 'UQ',
-				},
-			},
-		}
-		const expected = {
-			'0': 'Nordic Semiconductor ASA',
-			'1': 123,
-			'11': 'Thingy:91',
-			'16': 'UQ',
-		}
-
-		const result = removeCoioteFormatFromSingleInstanceObj(object)
-		assert.deepEqual(result, expected)
 	})
 })
