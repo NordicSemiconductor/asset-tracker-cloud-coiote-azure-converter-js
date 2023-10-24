5544c6a94e7878b793636f0cd28a0f473e6ad312
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-24 16:23:00 +0200

    test: check return value when notknown format in input
    explain expected result when input is not known in 'removeCoioteFormatFromSingleInstanceObj' method

--- src/utils/removeCoioteFormatFromSingleInstanceObj.spec.ts
+++ src/utils/removeCoioteFormatFromSingleInstanceObj.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
 import { removeCoioteFormatFromSingleInstanceObj } from './removeCoioteFormatFromSingleInstanceObj.js'
+import type { Instance } from './LwM2MCoioteType.js'
 
 void describe('removeCoioteFormatFromSingleInstanceObj', () => {
 	void it('should remove coiote format from single instance object', () => {
@@ -33,6 +34,37 @@ void describe('removeCoioteFormatFromSingleInstanceObj', () => {
 			'1': 'Thingy:91',
 			'11': [0, 0],
 			'16': 'UQ',
+		}
+
+		assert.deepEqual(removeCoioteFormatFromSingleInstanceObj(object), result)
+	})
+
+	void it(`should return undefined when resource does not follow Coiote format`, () => {
+		const object = {
+			'0': {
+				'0': {}, // uknown format here
+				'1': {
+					value: 'Thingy:91',
+				},
+				'11': {
+					'0': {}, // uknown format here
+					'1': {
+						value: 0,
+					},
+					attributes: {
+						dim: '2',
+					},
+				},
+				'16': {
+					values: 'UQ', // uknown format here
+				},
+			},
+		} as unknown as Instance
+		const result = {
+			'0': undefined,
+			'1': 'Thingy:91',
+			'11': [undefined, 0],
+			'16': undefined,
 		}
 
 		assert.deepEqual(removeCoioteFormatFromSingleInstanceObj(object), result)
