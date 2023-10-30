a349f70dba2cb9dadadcb1c68846298fdae0938b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-10-30 17:44:04 +0100

    refactor: do not copy type

--- src/utils/ValidationError.ts
+++ src/utils/ValidationError.ts
@@ -1,18 +1,12 @@
-type ErrorDescription = {
-	instancePath: string
-	schemaPath: string
-	keyword: string
-	params: Record<string, unknown>
-	message?: string
-}
+import { type ErrorObject } from 'ajv'
 
 /**
  * This error is returned when the converted object is not valid.
  */
 export class ValidationError extends Error {
-	description: ErrorDescription[]
+	description: ErrorObject[]
 
-	constructor(description: ErrorDescription[]) {
+	constructor(description: ErrorObject[]) {
 		super(`error validating type: ${JSON.stringify(description, null, 2)}`)
 		this.description = description
 	}
